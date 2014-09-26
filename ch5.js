afdfb6dd63d7084fa3de632b263eaa28492bfd36
David Ernst <hello@dsernst.com>
2014-09-26 13:28:44 -0500

    half attempts at exercise 2 & 3

--- ch5.js
+++ ch5.js
@@ -15,3 +15,94 @@ function flatten(array) {
 }
 
 
+// Exercise 2: Mother-Child Age Difference
+// this one's tough, looked at Hint.
+
+// Given function:
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+// Given object:
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+// Given function:
+function reduceAncestors(person, f, defaultValue) {
+  function valueFor(person) {
+    if (person == null)
+      return defaultValue;
+    else
+      return f(person, valueFor(byName[person.mother]),
+                       valueFor(byName[person.father]));
+  }
+  return valueFor(person);
+}
+
+
+function avgMaternalAge(dataSet) {
+  var ages = [];
+
+  function hasKnownMother(person){
+    if ( byName[ byName[person].mother ] ) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  function mothersAge(p){
+    return byName[p].mother.born - byName[p].born;
+  }
+
+  function whatsMothersAge(person){
+    ages.push(mothersAge(person))
+  }
+
+  byName.map(ages);
+
+  return average(ages);
+}
+
+
+// The above definitely doesn't work yet... skipping for now.
+
+
+// Exercise 3: Historical Life Expectancy
+// Goal: Find avg historical life expectancy by century
+// each person -> calc lifespan, group into century array
+// each century -> reduce to averages
+
+function deathCentury(person){
+  return Math.ceil(person.died / 100);
+}
+
+function lifespan(person){
+  return person.died - person.born;
+}
+
+function groupBy(array, func){
+  var sorted = {};
+
+  array.forEach( function(i){
+    if ( !sorted.hasOwnProperty( func(array[i]) ) ) {
+      sorted[ func(array[i]) ] = [ array[i] ];
+    }
+    else {
+      sorted[ func(array[i]) ].push( array[i] );
+    }
+  });
+
+  return sorted;
+}
+
+byName.map(lifespan)
+
+
+// argh not working yet either. this stuff is confusing.
+
